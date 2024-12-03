789d852459603e6046b99a3c8c0f823156ff5552
lmriccardo <riccardo.lamarca98@gmail.com>
2024-12-03 11:02:36 +0100

    feat: :construction: Arrow handling
    Arrow handling for moving the cursor in correct positions. by defaults when writing with the cursor at a position rather than the end of the string is insert in Insertion mode

--- src/objects/textbox.go
+++ src/objects/textbox.go
@@ -6,6 +6,19 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
+type Point struct {
+	X int // X Position
+	Y int // Y Position
+}
+
+// Mapping event keys arrow to direction (x, y)
+var ARROW_MAPPING map[tcell.Key]Point = map[tcell.Key]Point{
+	tcell.KeyUp:    {0, -1},
+	tcell.KeyDown:  {0, +1},
+	tcell.KeyLeft:  {-1, 0},
+	tcell.KeyRight: {+1, 0},
+}
+
 type TextBox struct {
 	rec         Rectangle // The rectangle of the text box
 	title       string    // The title of the text box (Optional)
@@ -54,8 +67,11 @@ func (tb *TextBox) Display(screen tcell.Screen) {
 
 // Check if the textbox collides with input coordinates
 func (tb *TextBox) IsColliding(x, y int) bool {
-	return ((x > tb.rec.Start_x && x < tb.rec.Start_x+tb.rec.Width) &&
-		(y > tb.rec.Start_y && y < tb.rec.Start_y+tb.rec.Height))
+	y_diff := tb.start_pos_y - tb.rec.Start_y - 1
+	x_diff := tb.start_pos_x - tb.rec.Start_x - 1
+
+	return ((x >= tb.rec.Start_x+x_diff && x <= tb.rec.Start_x+tb.rec.Width-x_diff) &&
+		(y >= tb.rec.Start_y+y_diff && y <= tb.rec.Start_y+tb.rec.Height-y_diff))
 }
 
 func (tb *TextBox) SetFocus(value bool) {
@@ -94,11 +110,16 @@ func (tb *TextBox) getMaxRowSize() int {
 	return tb.rec.Width - 2*(tb.start_pos_x-tb.rec.Start_x)
 }
 
-func (tb *TextBox) getStringPosition() int {
-	row_size := tb.getMaxRowSize()                       // Max row size
-	rel_cursor_x := tb.curr_pos_x - tb.start_pos_x       // Relative X cursor position
-	str_position := tb.curr_line*row_size + rel_cursor_x // Position into the string
+func (tb *TextBox) getStringPosition(x, y int) int {
+	curr_line := y - tb.start_pos_y
+	row_size := tb.getMaxRowSize()                    // Max row size
+	rel_cursor_x := x - tb.start_pos_x                // Relative X cursor position
+	str_position := curr_line*row_size + rel_cursor_x // Position into the string
 	return str_position
+}
+
+func (tb *TextBox) getCurrentStringPosition() int {
+	return tb.getStringPosition(tb.curr_pos_x, tb.curr_pos_y)
 }
 
 // func (tb *TextBox) getLastYStringPosition() int {
@@ -222,9 +243,9 @@ func (tb *TextBox) handleBackspace(screen tcell.Screen) {
 		return
 	}
 
-	str_position := tb.getStringPosition() // Take the position into the string
-	content_array := []rune(tb.content)    // From string to rule array
-	content_len := len(tb.content)         // Take the length of the string
+	str_position := tb.getCurrentStringPosition() // Take the position into the string
+	content_array := []rune(tb.content)           // From string to rule array
+	content_len := len(tb.content)                // Take the length of the string
 
 	// Check how many characters are left in the sx substr
 	substr_sx := content_array[0:str_position]
@@ -260,6 +281,38 @@ func (tb *TextBox) handleBackspace(screen tcell.Screen) {
 	tb.drawContent(screen)
 }
 
+func (tb *TextBox) handleArrowPressed(screen tcell.Screen, direction_x, direction_y int) {
+	next_pos_x := tb.curr_pos_x + direction_x
+	next_pos_y := tb.curr_pos_y + direction_y
+	next_str_pos := tb.getStringPosition(next_pos_x, next_pos_y)
+
+	// Handle next or previous line
+	// next_pos_x-tb.start_pos_x < 0 || next_pos_y-tb.start_pos_y < 0
+	if next_pos_x < tb.start_pos_x {
+		next_pos_x = tb.start_pos_x + tb.getMaxRowSize()
+		next_pos_y--
+	} else if next_pos_x >= tb.start_pos_x+tb.getMaxRowSize() {
+		next_pos_x = tb.start_pos_x
+		next_pos_y++
+	}
+
+	// Check overflow for Y positions
+	y_diff := tb.rec.Start_y + tb.rec.Height - (tb.start_pos_y - tb.rec.Start_y)
+	if next_pos_y < tb.start_pos_y || next_pos_y > y_diff {
+		return
+	}
+
+	// Check if the content is empty or not, or the new position
+	// is bigger than the dimension of the string
+	if len(tb.content) < 1 || next_str_pos > len(tb.content) {
+		return
+	}
+
+	// Set the new cursor position
+	tb.SetNextCursorPosition(next_pos_x, next_pos_y)
+	screen.ShowCursor(tb.curr_pos_x, tb.curr_pos_y)
+}
+
 func (tb *TextBox) HandleEventKey(screen tcell.Screen, event *tcell.EventKey) {
 	if !tb.focus {
 		return
@@ -278,11 +331,19 @@ func (tb *TextBox) HandleEventKey(screen tcell.Screen, event *tcell.EventKey) {
 		// tb.handleEnterKey(screen)
 
 	case tcell.KeyDelete:
+		// In the case the CANC key is pressed deletes the
+		// characther next to the current one
 
 	case tcell.KeyBackspace, tcell.KeyBackspace2:
 		// When backspace is pressed deletes the caracter where
 		// the cursor is positioned
 		tb.handleBackspace(screen)
+
+	case tcell.KeyUp, tcell.KeyDown, tcell.KeyLeft, tcell.KeyRight:
+		// When the arrow is pressed we need to select the correct
+		// move and update the cursor position if it is possible
+		direction := ARROW_MAPPING[event.Key()]
+		tb.handleArrowPressed(screen, direction.X, direction.Y)
 
 	default:
 		// Otherwise, check if the key pressed is a letter
